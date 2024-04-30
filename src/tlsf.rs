805ad6381b63f8d044c352e85f2f63503627d1c9
TheR1sing3un <ther1sing3un@163.com>
2024-04-30 23:51:45 +0800

    feat: support searching free block list

--- src/tlsf.rs
+++ src/tlsf.rs
@@ -62,6 +62,7 @@ struct FreeBlockHeader {
 }
 
 struct UsedBlockHeader {
+
     block_header: BlockHeader
 }
 
@@ -82,7 +83,7 @@ impl<const FLLEN: usize, const SLLEN: usize, const MIN_BLOCK_SIZE: usize>
 
     const SLI: usize = SLLEN.trailing_zeros() as usize;
 
-    pub fn map_search(&self, size: usize) -> Option<(usize/*fl-index*/, usize/*sl-index*/)> {
+    fn map_search(&self, size: usize) -> Option<(usize/*fl-index*/, usize/*sl-index*/)> {
         let fl_phy_index = (usize::BITS - size.leading_zeros()) as usize - 1;
         let fl = fl_phy_index - Self::MIN_BLOCK_SIZE_LOG2;
 
@@ -94,6 +95,43 @@ impl<const FLLEN: usize, const SLLEN: usize, const MIN_BLOCK_SIZE: usize>
         let mask = (1 << Self::SLI) - 1;
         let sl = mask & shifted_size;
         Some((fl, sl))
+    }
+
+    fn map_search_bigger(&self, size: usize) -> Option<(usize/*fl-index*/, usize/*sl-index*/)> {
+        let fl_phy_index = (usize::BITS - size.leading_zeros()) as usize - 1;
+        let fl = fl_phy_index - Self::MIN_BLOCK_SIZE_LOG2;
+
+        if fl >= FLLEN {
+            return None;
+        }
+
+        let shifted_size = size >> (fl_phy_index - Self::SLI);
+        let mask = (1 << Self::SLI) - 1;
+        let sl = mask & shifted_size;
+        if sl == SLLEN - 1 {
+            if fl == FLLEN - 1 {
+                return None;
+            }
+            return Some((fl + 1, 0));
+        }
+        Some((fl, sl + 1))
+    }
+
+    pub fn search_free_block(&self, min_size: usize) -> Option<(usize/*fl-index*/, usize/*sl-index*/)> {
+        let (fl, sl) = self.map_search_bigger(min_size)?;
+        if self.sl_bitmap[fl].get(sl) {
+            return Some((fl, sl));
+        }
+        if let Some(new_sl) = self.sl_bitmap[fl].next_index(sl) {
+            if new_sl < SLLEN {
+                return Some((fl, new_sl));
+            }
+        }
+        if let Some(new_fl) = self.fl_bitmap.next_index(fl) {
+            let new_sl = self.sl_bitmap[new_fl].first_index()?;
+            return Some((new_fl, new_sl));
+        }
+        None
     }
 
 }
@@ -118,5 +156,38 @@ mod tests {
         assert_eq!(tlsf.map_search(40), Some((1, 1)));
         assert_eq!(tlsf.map_search((1 << 32) - 1), Some((27, 3)));
         assert_eq!(tlsf.map_search(1 << 32), None);
+    }
+
+    #[test]
+    fn test_tlsf_map_search_bigger() {
+        let tlsf: Tlsf<28, 4, 16> = Tlsf::new();
+        assert_eq!(tlsf.map_search_bigger(16), Some((0, 1)));
+        assert_eq!(tlsf.map_search_bigger(17), Some((0, 1)));
+        assert_eq!(tlsf.map_search_bigger(20), Some((0, 2)));
+        assert_eq!(tlsf.map_search_bigger(31), Some((1, 0)));
+        assert_eq!(tlsf.map_search_bigger(32), Some((1, 1)));
+        assert_eq!(tlsf.map_search_bigger(40), Some((1, 2)));
+        assert_eq!(tlsf.map_search_bigger((1 << 32) - 1), None);
+    }
+
+    #[test]
+    fn test_tlsf_search_free_block() {
+        let mut tlsf: Tlsf<28, 4, 16> = Tlsf::new();
+
+        tlsf.fl_bitmap.set(2, true);
+        tlsf.sl_bitmap[2].set(1, true);
+        tlsf.sl_bitmap[2].set(3, true);
+
+        tlsf.fl_bitmap.set(4, true);
+        tlsf.sl_bitmap[4].set(0, true);
+        tlsf.sl_bitmap[4].set(1, true);
+
+        assert_eq!(tlsf.search_free_block(16), Some((2, 1)));
+        assert_eq!(tlsf.search_free_block(80), Some((2, 3)));
+        assert_eq!(tlsf.search_free_block(112), Some((4, 0)));
+        assert_eq!(tlsf.search_free_block(128), Some((4, 0)));
+        assert_eq!(tlsf.search_free_block(255), Some((4, 0)));
+        assert_eq!(tlsf.search_free_block(256), Some((4, 1)));
+        assert_eq!(tlsf.search_free_block(320), None);
     }
 }
